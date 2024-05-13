1f9b7a4f92ef287886736bcf9cbf164f008938c7
Yoseph <yoseph1977@yahoo.com>
2024-05-13 10:21:35 +0200

    deployment start

---
+++ src/main/java/com/polarbooksop/catalogservice/config/PolarProperties.java
@@ -0,0 +1,14 @@
+package com.polarbooksop.catalogservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "polar")
+@Getter
+@Setter
+public class PolarProperties {
+
+    private String greeting;
+
+}
